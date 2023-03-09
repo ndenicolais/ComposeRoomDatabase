32db10e0390ee08d62cc8f1f97fc54614638f98a
ndenicolais <n.denicolais@arethusasrl.it>
2023-03-09 10:59:02 +0100

    Added

--- app/src/main/java/com/denicks21/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/denicks21/roomdatabase/MainActivity.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.ModalDrawer
+import androidx.compose.material.Surface
+import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.SideEffect
-import com.denicks21.roomdatabase.screen.MainPage
+import androidx.compose.runtime.rememberCoroutineScope
+import com.denicks21.roomdatabase.navigation.NavGraph
+import com.denicks21.roomdatabase.navigation.NavScreens
+import com.denicks21.roomdatabase.ui.composables.CustomDrawer
 import com.denicks21.roomdatabase.ui.theme.GreyDark
 import com.denicks21.roomdatabase.ui.theme.RoomDatabaseTheme
 import com.denicks21.roomdatabase.viewmodels.HomeViewModel
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val homeViewModel: HomeViewModel by viewModels()
 
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -27,7 +38,46 @@ class MainActivity : ComponentActivity() {
                         darkIcons = false
                     )
                 }
-                MainPage(homeViewModel)
+                val navController = rememberAnimatedNavController()
+                Surface {
+                    val drawerState = rememberDrawerState(DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+                    val openDrawer = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }
+                    ModalDrawer(
+                        drawerState = drawerState,
+                        gesturesEnabled = drawerState.isOpen,
+                        drawerContent = {
+                            CustomDrawer(
+                                onDestinationClicked = { route ->
+                                    scope.launch {
+                                        drawerState.close()
+                                    }
+                                    navController.navigate(route) {
+                                        navController.graph.startDestinationRoute?.let {
+                                            popUpTo(NavScreens.UserListPage.route) {
+                                                saveState = true
+                                            }
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                }
+                            )
+                        }
+                    ) {
+                        NavGraph(
+                            navController,
+                            homeViewModel,
+                            openDrawer = {
+                                openDrawer()
+                            }
+                        )
+                    }
+                }
             }
         }
     }
